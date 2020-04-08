8de4df081ed926737d6bac60ef308ca4703c5ad2
Torran Green <torrangreen@hotmail.co.uk>
2020-04-08 11:30:29 +0100

    google attempt 2

--- sql_test.py
+++ sql_test.py
@@ -5,7 +5,7 @@ import pandas as pd
 from datetime import datetime
 import os
 
-# os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=r"config\my-covid-project.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="my-covid-project.json"
 
 
 class CovidQuery:
